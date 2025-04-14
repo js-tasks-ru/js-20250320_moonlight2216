b58d27c04a80c4be0f3ebf444c61ea493eac7b3f
olga160990 <moonlight2216@yahoo.com>
2025-04-15 00:24:34 +0300

    NotificationMessage component

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,41 @@
 export default class NotificationMessage {
+  constructor(message = "", props = {}) {
+    const { duration = 0, type = "" } = props;
+    this.header = "Notification";
+    this.type = type;
+    this.duration = duration ?? 1000;
+    this.message = message;
+    this.element = null;
+    this.timerId = null;
+    this.show();
+  }
 
+  show() {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = `<div class="notification ${this.type}" style="--value:20s">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.header}</div>
+      <div class="notification-body">
+        ${this.message}
+      </div>
+    </div>
+  </div>`;
+    this.element = wrapper.firstElementChild;
+    const body = document.querySelector("body");
+    if (this.element) {
+      body.append(this.element);
+    }
+  }
+
+  destroy() {
+    if (this.element) {
+      this.element.remove();
+      this.element = null;
+    }
+  }
+
+  remove() {
+    this.timerId = setTimeout(() => this.destroy(), this.duration);
+  }
 }
